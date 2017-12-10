60e3b3a79d807dcfb43c420657bbfd1fbcbe6fdf
Elliott N <elliottning@gmail.com>
2017-12-09 22:14:11 -0800

    Update search.php

--- search.php
+++ search.php
@@ -6,22 +6,22 @@ define('DB_NAME', 'gcp');
 
 
 if (isset($_GET['term'])){
-	$return_arr = array();
+        $return_arr = array();
 
-	try {
-	    $conn = new PDO("mysql:host=".DB_SERVER.";port=8889;dbname=".DB_NAME, DB_USER, DB_PASSWORD);
-	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	    
-	    $stmt = $conn->prepare('SELECT country FROM countries WHERE country LIKE :term');
-	    $stmt->execute(array('term' => '%'.$_GET['term'].'%'));
-	    
-	    while($row = $stmt->fetch()) {
-	        $return_arr[] =  $row['country'];
-	    }
+        try {
+            $conn = new PDO("mysql:host=".DB_SERVER.";port=8889;dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	} catch(PDOException $e) {
-	    echo 'ERROR: ' . $e->getMessage();
-	}
+            $stmt = $conn->prepare('SELECT product FROM products WHERE product LIKE :term LIMIT 20');
+            $stmt->execute(array('term' => '%'.$_GET['term'].'%'));
+
+            while($row = $stmt->fetch()) {
+                $return_arr[] =  $row['product'];
+            }
+
+        } catch(PDOException $e) {
+            echo 'ERROR: ' . $e->getMessage();
+        }
 
 
     /* Toss back results as json encoded array. */
