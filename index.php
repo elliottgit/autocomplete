61a442aec741cfbf16ac2233b52c64135366775b
Elliott N <elliottning@gmail.com>
2017-12-10 00:07:29 -0800

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,10 @@
 </head>
 <body>
 
-        <form action='' method='post'>
-                <p><label>Product:</label><input type='text' name='product' value='' class='auto'></p>
-        </form>
+    <form action='' method='post'>  
+			<p><label>Product:</label><input type='text' name='product' value='' class='auto'></p>
+			<p style="text-align: center;"><img src="home.jpg" alt="Home Image" width="450" height="400" /></p>  
+    </form>
 
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 <script type="text/javascript" src="http://code.jquery.com/ui/1.10.1/jquery-ui.min.js"></script>
@@ -21,7 +22,6 @@ $(function() {
                 source: "search.php",
                 minLength: 1
         });                             
-
 });
 </script>
 </body>
