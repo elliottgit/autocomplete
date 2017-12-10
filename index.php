909f136acd5be7580c1304441507e114d5a8f17f
Elliott N <elliottning@gmail.com>
2017-12-10 00:12:11 -0800

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,13 @@
 <body>
 
     <form action='' method='post'>  
-			<p><label>Product:</label><input type='text' name='product' value='' class='auto'></p>
-			<p style="text-align: center;"><img src="home.jpg" alt="Home Image" width="450" height="400" /></p>  
+	<p><label>Product:</label><input type='text' name='product' value='' class='auto'></p>
+	<p style="text-align: center;"><img src="home.jpg" alt="Home Image" width="450" height="400" /></p>  
     </form>
-
+    <br>
+    <hr />
+    <p><strong> Author: Elliott Ning </strong></p>
+    <p><strong> Date: 12/12/2017 </strong></p>
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 <script type="text/javascript" src="http://code.jquery.com/ui/1.10.1/jquery-ui.min.js"></script>
 <script type="text/javascript">
